5914411cf035cf7c66e751f5a68e67d6d96684f8
i2513484-droid <i2513484@continental.edu.pe>
2025-12-21 15:28:30 -0500

    Update pPersona.py

--- capaPresentacion/pPersona.py
+++ capaPresentacion/pPersona.py
@@ -13,25 +13,25 @@ class PPersona:
         if 'nombre_seccion' not in st.session_state:
             st.session_state.Nombre_seccion = ''
         if 'edad_seccion' not in st.session_state:
-            st.session_state.edad_seccion = ''
+            st.session_state.Edad_seccion = ''
         if 'telefono_seccion' not in st.session_state:
-            st.session_state.telefono_seccion = ''
+            st.session_state.Telefono_seccion = ''
         if 'correo_seccion'not in st.session_state:
-            st.session_state.correo_seccion = ''
+            st.session_state.Correo_seccion = ''
         self.__construirInterfaz()
 
     def __construirInterfaz(self):
         st.title('Bienvenido a TAYTA SHANTI')
-        if st.session_state.persona_seleccionada !='':
+        if st.session_state.persona_Seleccionada !='':
             st.session_state.docIdentdidad_sesion = st.session_state.persona_seleccionada['docIdentidad']
             st.session_state.nombre_sesion = st.session_state.persona_seleccionada ['nombre']
             st.session_state.edad_sesion = st.session_state.persona_seleccionada['edad']
             st.session_state.telefono_seccion = st.session_state.persona_selecionada['telefono']
             st.session_state.correo.seccion = st.session_state.persona_seleccionada['correo']
         with st.form(f'FormularioPersona{st.session_state.formularioKey}'):
-            txtDocIdentidad = st.text_input('Documento de identidad', value=st.session_state.docIdentidad_seccion)
-            txtNombre = st.text_input('Nombre', value=st.session_state.nombre_seccion)
-            txtEdad = st.number_input('Edad', min_value = 0, max_value = 150, value=st.session_state.edad_seccion)
+            txtDocIdentidad = st.text_input('Documento de identidad', value =st.session_state.docIdentidad_seccion)
+            txtNombre = st.text_input('Nombre', value=st.session_state.Nombre_seccion)
+            txtEdad = st.number_input('Edad', min_value=0, max_value=150, value=st.session_state.edad_seccion)
             txtTelefono = st.text_input('Telefono', value=st.session_state.telefono_seccion)
             txtCorreo = st.text_input('Correo', value=st.session_state.correo_seccion)
             if st.session_state.persona_seleccionada != '':
@@ -39,15 +39,15 @@ class PPersona:
             else:
                 btnGuardar = st.form_submit_button('Guardar', type = 'primary')
            
-                if btnGuardar:
-                    persona = {
-                        'Documento de identidad': txtDocIdentidad,
-                        'Nombre': txtNombre,
-                        'Edad': txtEdad,
-                        'Telefono': txtTelefono,
-                        'Correo': txtCorreo
-                    }
-                    self.nuevaPersona(persona)
+            if btnGuardar:
+                persona = {
+                    'Documento de identidad': txtDocIdentidad,
+                    'Nombre': txtNombre,
+                    'Edad': txtEdad,
+                    'Telefono': txtTelefono,
+                    'Correo': txtCorreo
+                }
+                self.nuevaPersona(persona)
         self.mostrarPersonas()
 
     def mostrarPersonas(self):
